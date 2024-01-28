2fb7a0e4c39c886afe0a6a1d0201a176f2b2c158
SofiiaKozlyk <ipz222_kso@student.ztu.edu.ua>
2024-01-28 20:12:26 +0200

    Added .gitignore and C# console project that calculates the sum of two numbers

---
+++ Task0/Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+ System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+customCulture.NumberFormat.NumberDecimalSeparator = ".";
+System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+bool ok = false;
+double x, y;
+do
+{
+    Console.Write("Enter the first number: ");
+    ok = double.TryParse(Console.ReadLine(), out x);
+    if (!ok) Console.WriteLine("Please enter a number!");
+} while (!ok);
+do
+{
+    Console.Write("Enter the second number: ");
+    ok = double.TryParse(Console.ReadLine(), out y);
+    if (!ok) Console.WriteLine("Please enter a number!");
+} while (!ok);
+Console.WriteLine($"Sum: {x + y}");
